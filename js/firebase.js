348205278dc659c79b7b81cd306b52ac46be8891
nisha666806-debug <nisha666806@gmail.com>
2026-09-23 05:36:46 +0500

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,74 @@
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  serverTimestamp,
+  runTransaction,
+  writeBatch
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCr2AynK-aXLHmKoq8lVU7tb49U-V1TVG0",
+  authDomain: "khobgoh-23dcc.firebaseapp.com",
+  projectId: "khobgoh-23dcc",
+  storageBucket: "khobgoh-23dcc.firebasestorage.app",
+  messagingSenderId: "154064833176",
+  appId: "1:154064833176:web:a4a2f002a2783ecd5fdeed"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const secondaryApp = initializeApp(firebaseConfig, "SupervisorRegistrationApp");
+const registrationAuth = getAuth(secondaryApp);
+const registrationDb = getFirestore(secondaryApp);
+
+export {
+  app,
+  auth,
+  db,
+  registrationAuth,
+  registrationDb,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  serverTimestamp,
+  runTransaction,
+  writeBatch
+};
